6ebac2e74be64c0d29d4e24f4e83cb6786b29415
Lucas McGill <lmlucasmc8@gmail.com>
2022-01-14 16:03:37 -0500

    oops

--- bash.js
+++ bash.js
@@ -1,15 +1,30 @@
-process.stdout.write("prompt> ");
+process.stdout.write("prompt > ");
 
 const pwd = require("./pwd");
-
+const cat = require("./cat")
 const ls = require("./ls");
+const curl = require('./curl')
 
 process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
   if (cmd === "pwd") {
-    pwd();
+    pwd(done);
   }
   if (cmd === "ls") {
-    ls();
+    ls(done);
+  }
+  if(cmd.split(' ')[0] === 'cat'){
+    cat(`${process.cwd()}/${cmd.split(' ')[1]}`, done)
+  }
+  if(cmd.split(' ')[0] === 'curl'){
+    curl(cmd.split(' ')[1], done)
   }
 });
+
+
+const done = (output) => {
+  process.stdout.write(output)
+  process.stdout.write("\nprompt > ");
+}
+
+module.exports = done;
